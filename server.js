93d0f43a61ca5e069080cc1655f218dd13888cb0
nmartin <nate.martin27@gmail.com>
2024-11-12 18:31:28 -0500

    Styling: phase 1

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+app.use(express.static('public'));
 
 const welcomeMessage =
-  "Welcome to Memecorp, the best place to hire famous Meme People for taks.";
+  "Welcome to Memecorp, the best place to hire famous Meme People for tasks.";
 const historyTxt =
   "This company has a history, the best history, a long history of doing stuff for people. I promise. Do not think about it too much.";
 const staffData = [
