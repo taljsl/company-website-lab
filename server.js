e904d592f47ebdb39b19919d1dbcb7d1c5eab632
Josh L <joshua.levine42@gmail.com>
2024-11-12 18:34:04 -0500

    required to commit before able to pull changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.static('public'));
 
 const welcomeMessage =
   "Welcome to Memecorp, the best place to hire famous Meme People for taks.";
